75aa273fa9315b392107ea8ff3da5b5712e1cf0b
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-30 11:29:38 +0530

    Added: Ability to show sum of three integers adds upto 0

---
+++ SumOfThreeIntegers.js
@@ -0,0 +1,11 @@
+var array = [];
+var sum = 0 ;
+var integer1 = parseInt(process.argv[2]);
+var integer2 = parseInt(process.argv[3]);
+var integer3 = parseInt(process.argv[4]);
+array.push(integer1,integer2,integer3);
+console.log("The array is: " + array);
+array.forEach((element) => {
+    sum+=element;
+});
+console.log("The sum of three integers is: " + sum);
